aaec1d6a37b55b62ed0c7fe002975fc8b192734d
bastianplsfix <hello@bastian.sh>
2023-12-09 20:58:56 +0100

    Outlet improvements

--- src/Outlet.jsx
+++ src/Outlet.jsx
@@ -1,13 +1,16 @@
-import {createRouter} from "@nanostores/router"
-import {useStore} from '@nanostores/react'
-import {Home} from "./modules/Home/index.js"
-import {Containers} from "./modules/Grid/Containers/index.js";
-import {Property} from "./modules/Grid/Property/index.js";
-import {TrackSizing} from "./modules/Grid/TrackSizing/index.js";
-import {FractionUnit} from "./modules/Grid/FractionUnit/index.js";
-import {Minmax} from "src/modules/Grid/Minmax/index.js";
-import {MinAndMaxContent} from "src/modules/Grid/MinAndMaxContent/index.js";
-import {FitContent} from "src/modules/Grid/FitContent/index.js";
+import React from 'react';
+import { createRouter } from "@nanostores/router";
+import { useStore } from '@nanostores/react';
+
+// Lazy import components
+const Home = React.lazy(() => import("./modules/Home/index.js").then(module => ({ default: module.Home })));
+const Containers = React.lazy(() => import("./modules/Grid/Containers/index.js").then(module => ({ default: module.Containers })));
+const Property = React.lazy(() => import("./modules/Grid/Property/index.js").then(module => ({ default: module.Property })));
+const TrackSizing = React.lazy(() => import("./modules/Grid/TrackSizing/index.js").then(module => ({ default: module.TrackSizing })));
+const FractionUnit = React.lazy(() => import("./modules/Grid/FractionUnit/index.js").then(module => ({ default: module.FractionUnit })));
+const Minmax = React.lazy(() => import("src/modules/Grid/Minmax/index.js").then(module => ({ default: module.Minmax })));
+const MinAndMaxContent = React.lazy(() => import("src/modules/Grid/MinAndMaxContent/index.js").then(module => ({ default: module.MinAndMaxContent })));
+const FitContent = React.lazy(() => import("src/modules/Grid/FitContent/index.js").then(module => ({ default: module.FitContent })));
 
 export const $router = createRouter({
     home: '/',
@@ -18,28 +21,29 @@ export const $router = createRouter({
     gridMinMax: "/grid/minmax",
     gridMinAndMaxContent: "/grid/min-and-max-content",
     gridFitContent: "/grid/fit-content",
-})
+});
+
+const RouteToComponent = {
+    home: Home,
+    gridContainers: Containers,
+    gridProperty: Property,
+    gridTrackSizing: TrackSizing,
+    gridFractionUnit: FractionUnit,
+    gridMinMax: Minmax,
+    gridMinAndMaxContent: MinAndMaxContent,
+    gridFitContent: FitContent,
+};
 
 export const Outlet = () => {
-    const page = useStore($router)
+    const page = useStore($router);
+    const Component = RouteToComponent[page?.route];
 
-    if (!page) {
-        return <div>Not found</div>
-    } else if (page.route === 'home') {
-        return <Home />
-    } else if (page.route === "gridContainers") {
-        return <Containers />
-    } else if (page.route === "gridProperty") {
-        return <Property />
-    } else if (page.route === "gridTrackSizing") {
-        return <TrackSizing />
-    } else if (page.route === "gridFractionUnit") {
-        return <FractionUnit />
-    } else if (page.route === "gridMinMax") {
-        return <Minmax />
-    } else if (page.route === "gridMinAndMaxContent") {
-        return <MinAndMaxContent />
-    } else if (page.route === "gridFitContent") {
-        return <FitContent />
-    }
-}
+    return (
+        <React.Suspense fallback={<div>Loading...</div>}>
+            { Component
+                ? <Component />
+                : <div>Not found</div>
+            }
+        </ React.Suspense>
+    );
+};
